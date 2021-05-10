2e877199cc6ab1589fed98620b5131b2955446bc
KoryB <byrnek@mymail.shawnee.edu>
2021-05-10 18:45:25 -0400

    Fragment mode shader now works with cpu-side pixel units

--- components/metaballs_grid/scripts/metaballs_grid.gd
+++ components/metaballs_grid/scripts/metaballs_grid.gd
@@ -11,9 +11,9 @@ const FragmentModeShaderMaterial := preload("../materials/fragment_render_mode_s
 enum RenderMode { MARCHING_SQUARES, VERTEX_COLORS, FRAGMENT_COLORS };
 
 
-export var grid_cells := Vector2(32, 32);
+export var grid_cells := Vector2(32, 32) setget _set_grid_cells;
+export var grid_size := Vector2(256, 256) setget _set_grid_size;
 export(RenderMode) var render_mode := RenderMode.FRAGMENT_COLORS setget _set_render_mode;
-
 
 var _builder: MetaballsGridMeshBuilder;
 var _is_children_different := false;
@@ -21,6 +21,9 @@ var _circles_cached := [];
 
 # Uniforms
 var _local_cell_size: Vector2;
+var _circle_position_offset := Vector2(0, 0);
+var _circle_position_scale := Vector2(0, 0);
+var _circle_radius_squared_scale := 0.0;
 var _circles_position_radius_squared_image: Image;
 var _circles_position_radius_squared_texture: ImageTexture;
 
@@ -36,26 +39,20 @@ func _init_render_mode():
 	_builder = MetaballsGridMeshBuilder.new();
 
 	if render_mode == RenderMode.MARCHING_SQUARES:
-		mesh = _builder.build_mesh(grid_cells);
-
-		_local_cell_size.x = 1.0 / grid_cells.x;
-		_local_cell_size.y = 1.0 / grid_cells.y;
+		mesh = _builder.build_mesh(grid_cells, grid_size);
+		_local_cell_size = grid_size / grid_cells;
 
 		material = MarchingSquaresModeShaderMaterial
 
 	elif render_mode == RenderMode.VERTEX_COLORS:
-		mesh = _builder.build_mesh(grid_cells);
-
-		_local_cell_size.x = 1.0 / grid_cells.x;
-		_local_cell_size.y = 1.0 / grid_cells.y;
+		mesh = _builder.build_mesh(grid_cells, grid_size);
+		_local_cell_size = grid_size / grid_cells;
 
 		material = VertexModeShaderMaterial;
 
 	elif render_mode == RenderMode.FRAGMENT_COLORS:
-		mesh = _builder.build_mesh(Vector2(1, 1));
-
-		_local_cell_size.x = 1.0;
-		_local_cell_size.y = 1.0;
+		mesh = _builder.build_mesh(Vector2(1, 1), grid_size);
+		_local_cell_size = Vector2(1, 1);
 
 		material = FragmentModeShaderMaterial;
 
@@ -74,11 +71,14 @@ func _init_circle_data():
 
 func _process(_delta):
 	material.set_shader_param("u_local_cell_size", _local_cell_size);
+
+	material.set_shader_param("u_circle_position_offset", _circle_position_offset);
+	material.set_shader_param("u_circle_position_scale", _circle_position_scale);
+	material.set_shader_param("u_circle_radius_squared_scale", _circle_radius_squared_scale);
 	material.set_shader_param('u_circles_position_radius_squared', _circles_position_radius_squared_texture);
 	material.set_shader_param('u_circle_count', get_circle_count());
 
 	_sync_circle_data();
-
 
 
 func get_circles() -> Array:
@@ -114,16 +114,30 @@ func _sync_circle_data():
 
 
 func _blit_circle_data_to_image():
+	_calculate_circle_transform_uniforms();
+
 	var circles = get_circles();
 
 	for index in range(circles.size()):
 		var circle: Metaball = circles[index];
-		var pixel := circle.get_pixel()
+		var pixel := _get_circle_pixel_transformed(circle);
 				
 		_circles_position_radius_squared_image.lock();
 		_circles_position_radius_squared_image.set_pixel(index, 0, pixel);
 		_circles_position_radius_squared_image.unlock();
 
+
+func _set_grid_cells(new_grid_cells: Vector2):
+	if not grid_cells.is_equal_approx(new_grid_cells):
+		grid_cells = new_grid_cells;
+		_init_render_mode();
+	
+
+func _set_grid_size(new_grid_size: Vector2):
+	if not grid_size.is_equal_approx(new_grid_size):
+		grid_size = new_grid_size;
+		_init_render_mode();
+		
 
 func _set_render_mode(new_render_mode: int):
 	if new_render_mode != render_mode:
@@ -133,6 +147,36 @@ func _set_render_mode(new_render_mode: int):
 
 func _is_valid_metaball(o: Object) -> bool:
 	return o.has_method("get_pixel");
+
+
+func _calculate_circle_transform_uniforms():
+	var min_position = Vector2(INF, INF);
+	var max_position = Vector2(-INF, -INF);
+	var max_radius_squared = 0;
+
+	for circle in get_circles():
+		var raw_pixel = circle.get_pixel();
+		var pos = Vector2(raw_pixel.r, raw_pixel.g);
+		var radius_squared = raw_pixel.b;
+
+		min_position = minv(min_position, pos);
+		max_position = maxv(max_position, pos);
+		max_radius_squared = max(max_radius_squared, radius_squared);
+
+	_circle_position_offset = min_position;
+	_circle_position_scale = max_position - min_position;
+	_circle_radius_squared_scale = max_radius_squared;
+
+
+func _get_circle_pixel_transformed(circle: Object) -> Color:
+	var raw_pixel = circle.get_pixel();
+	var position = Vector2(raw_pixel.r, raw_pixel.g);
+	var radius_squared = raw_pixel.b;
+
+	var transformed_position = (position - _circle_position_offset) / _circle_position_scale;
+	var transformed_radius_squared = radius_squared / _circle_radius_squared_scale;
+
+	return Color(transformed_position.x, transformed_position.y, transformed_radius_squared, raw_pixel.a);
 
 
 # Overrides
@@ -146,3 +190,12 @@ func remove_child(node: Node) -> void:
 	.remove_child(node);
 
 	_is_children_different = true;
+
+
+# Math
+func maxv(v1: Vector2, v2: Vector2) -> Vector2:
+	return Vector2(max(v1.x, v2.x), max(v1.y, v2.y));
+
+
+func minv(v1: Vector2, v2: Vector2) -> Vector2:
+	return Vector2(min(v1.x, v2.x), min(v1.y, v2.y));
--- components/metaballs_grid/scripts/metaballs_grid_mesh_builder.gd
+++ components/metaballs_grid/scripts/metaballs_grid_mesh_builder.gd
@@ -5,66 +5,67 @@ var _surface_tool: SurfaceTool;
 var _vertex_count: int;
 
 const square_offsets = [
-    Vector2(0, 0),
-    Vector2(1, 0),
-    Vector2(0, 1),
-    Vector2(1, 1),
+	Vector2(0, 0),
+	Vector2(1, 0),
+	Vector2(0, 1),
+	Vector2(1, 1),
 ]
 
 const index_offsets = [0, 1, 2, 2, 3, 1] # clockwise winding
 
 
 func _init():
-    _surface_tool = SurfaceTool.new();
-    _vertex_count = 0;
+	_surface_tool = SurfaceTool.new();
+	_vertex_count = 0;
 
 
-func build_mesh(grid_cells: Vector2, color:Color = Color.white) -> Mesh:
-    _initialize_grid(color);
-    _build_grid(grid_cells);
-    _finalize_grid();
+func build_mesh(grid_cells: Vector2, grid_size: Vector2, color:Color = Color.white) -> Mesh:
+	_initialize_grid(color);
+	_build_grid(grid_cells, grid_size);
+	_finalize_grid();
 
-    return _surface_tool.commit();
+	return _surface_tool.commit();
 
 
 func _initialize_grid(color: Color):
-    _vertex_count = 0;
+	_vertex_count = 0;
 
-    _surface_tool.clear();
-    _surface_tool.begin(Mesh.PRIMITIVE_TRIANGLES);
-    _surface_tool.add_color(color);
-
-
-func _build_grid(grid_cells: Vector2):
-    for x in range(grid_cells.x):
-        for y in range(grid_cells.y):
-            _build_grid_cell(Vector2(x, y), grid_cells);
+	_surface_tool.clear();
+	_surface_tool.begin(Mesh.PRIMITIVE_TRIANGLES);
+	_surface_tool.add_color(color);
 
 
-func _build_grid_cell(cell_position: Vector2, grid_cells: Vector2):
-    _add_grid_cell_indices()
-    _add_grid_cell_vertices(cell_position, grid_cells);
+func _build_grid(grid_cells: Vector2, grid_size: Vector2):
+	for x in range(grid_cells.x):
+		for y in range(grid_cells.y):
+			_build_grid_cell(Vector2(x, y), grid_cells, grid_size);
 
 
-func _add_grid_cell_vertices(cell_position: Vector2, grid_cells: Vector2):
-    for offset in square_offsets:
-        var position_normalized = cell_position + offset;
-        position_normalized.x /= grid_cells.x;
-        position_normalized.y /= grid_cells.y;
+func _build_grid_cell(cell_position: Vector2, grid_cells: Vector2, grid_size: Vector2):
+	_add_grid_cell_indices()
+	_add_grid_cell_vertices(cell_position, grid_cells, grid_size);
 
-        var vertex = Vector3(position_normalized.x, position_normalized.y, 0);
 
-        _surface_tool.add_uv(offset);
-        _surface_tool.add_vertex(vertex);
+func _add_grid_cell_vertices(cell_position: Vector2, grid_cells: Vector2, grid_size: Vector2):
+	for offset in square_offsets:
+		var position_normalized = cell_position + offset;
+		position_normalized.x /= grid_cells.x;
+		position_normalized.y /= grid_cells.y;
+		
+		var vertex_position = grid_size * position_normalized;
+		var vertex = Vector3(vertex_position.x, vertex_position.y, 0);
 
-        _vertex_count += 1;
+		_surface_tool.add_uv(offset);
+		_surface_tool.add_vertex(vertex);
+
+		_vertex_count += 1;
 
 
 
 func _add_grid_cell_indices():
-    for offset in index_offsets:
-        _surface_tool.add_index(_vertex_count + offset)
+	for offset in index_offsets:
+		_surface_tool.add_index(_vertex_count + offset)
 
 
 func _finalize_grid():
-    _surface_tool.index();
+	_surface_tool.index();
